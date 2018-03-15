a7a26fc66653cdd98a5f86790901055039de2c96
Shammi Hans <shammihans1@gmail.com>
2018-03-15 11:53:26 +0530

    generateOTP api

--- API/routes/login.js
+++ API/routes/login.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
-// this url is 'login/'
+
+// this url is relatively 'login/'
 router.get('/', function (req, res, next) {
     res.send('I2H RESTful API');
 });
+
+router.post('/generateOtp', function (req, res, next) {
+    const otpObj = {
+        otp : generateOTP(6),
+        mobNo : req.body.mobNo
+    };
+    console.log('OTP generated: ', otpObj.otp, ' for mobile no:', otpObj.mobNo);
+    // send otpObj.otp on SMS for mobNo
+    res.send(otpObj);
+});
+
+function generateOTP(digits) {
+    const adder = Math.pow(10, (digits-1));
+    const multiplier = Math.random() * 9 * Math.pow(10, (digits - 1));
+    const otp = Math.floor(adder + multiplier);
+    return otp;
+}
+
+// this should be in UI
+function deleteOtpObjLater() {
+    setTimeout(function () { otpObj.otp = null}, 1000 * 60 * 5);
+}
 
 module.exports = router;
--- API/server.js
+++ API/server.js
@@ -43,6 +43,7 @@ app.use(morgan('combined', {
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(bodyParser.json());
 
 /*
 // http://13.58.89.64:9000/auth/key-validity?resetKey=b5042e6b041747fb86296c5447b31b8
@@ -90,14 +91,9 @@ app.get(configs.pwdResetUrl, function (req, res) {
 
 app.use('/login', login);
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  console.log('catch 404 and forward to error handler');
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res) {
+  console.log('ERROR 404, URL NOT FOUND');
   res.send('ERROR 404, URL NOT FOUND');
-  // res.sendFile(path.join(staticLocation, 'index.html'));
-  // res.redirect(configs.dbUrl);
 });
 
 
