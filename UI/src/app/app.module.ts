5f1ced14791fd714e53737af721b14b974bc48f2
Shammi Hans <shammihans1@gmail.com>
2018-03-16 14:41:33 +0530

    removed mobile number input box, not needed

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
 import { LabelInputComponent } from './label-input/label-input.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
-    LabelInputComponent
+    LabelInputComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
