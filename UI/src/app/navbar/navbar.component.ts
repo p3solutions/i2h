fb00b58f0f37231aa5c4ad7fdc66ae81ebd4ee8e
Shammi Hans <shammihans1@gmail.com>
2018-03-28 09:35:20 +0530

    Logout fn

--- UI/src/app/navbar/navbar.component.ts
+++ UI/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { UserInfoService } from '../userinfo.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   info: any;
 
-  constructor() { }
+  constructor(
+    private auth: AuthenticationService,
+    private userInfoService: UserInfoService
+  ) { }
 
   ngOnInit() {
+    console.log('navbar loaded', this.auth.getUserDetails() );
+  }
+  getUserInfo() {
+    const email = this.auth.getLoggedInUserEmail();
+    this.userInfoService.getUserInfo(email).subscribe((res) => {
+      // this.info = res.data;
+      console.log(res, '<-userInfo');
+    });
   }
 
+  logOut() {
+    this.auth.logout();
+  }
 }
