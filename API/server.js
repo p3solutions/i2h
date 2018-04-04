dc7546da75c0f957d9c47f23fb91758d8f833126
Shammi Hans <shammihans1@gmail.com>
2018-04-04 13:32:39 +0530

    uncommenting allow header

--- API/server.js
+++ API/server.js
@@ -37,11 +37,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Add headers
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*"); // allow all origin to access
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // allow all origin to access
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // app.use(function (req, res, next) {
 //   // Website you wish to allow to connect
 //   res.setHeader('Access-Control-Allow-Origin', configs.IPallowedForUI);
