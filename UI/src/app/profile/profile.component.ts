72af53e08f611ca871c80d46f827b4a82a8cc653
Shammi Hans <shammihans1@gmail.com>
2018-04-06 20:52:10 +0530

    handling profile page

--- UI/src/app/profile/profile.component.ts
+++ UI/src/app/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonUtilityService } from '../common-utility.service';
+import { UserDetails, AuthenticationService } from '../authentication.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-profile',
@@ -8,26 +10,47 @@ import { CommonUtilityService } from '../common-utility.service';
 })
 export class ProfileComponent implements OnInit, OnDestroy {
   componentIcon = 'fa-user-circle';
+  userInfo: UserDetails;
+  loaderImage = environment.loaderImage;
 
   constructor(
+    private auth: AuthenticationService,
     private commonUtilityService: CommonUtilityService
-  ) { }
+  ) {
+    console.log('prof loaded');
+   }
 
   ngOnInit() {
     this.commonUtilityService.sendData(this.componentIcon);
+    this.auth.profile().subscribe(user => {
+      this.userInfo = user;
+      console.log(this.userInfo);
+    }, (err) => {
+      this.commonUtilityService.getErrorNotification(err);
+    });
   }
 
   sendData() {
     this.commonUtilityService.sendData(this.componentIcon);
   }
 
+  // clear message
   ngOnDestroy() {
-    // clear message
     this.commonUtilityService.clearData();
   }
 
+  // clear message
   clearData() {
-    // clear message
     this.commonUtilityService.clearData();
+  }
+
+  handleLabelName(e) {
+    this.commonUtilityService.handleInputLabelName(e);
+  }
+  allowOnlyNumbers(e) {
+    this.commonUtilityService.allowOnlyNumbers(e);
+  }
+  pasteOnlyNumbers(e) {
+    this.commonUtilityService.pasteOnlyNumbers(e);
   }
 }
--- UI/src/app/settings/settings.component.ts
+++ UI/src/app/settings/settings.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { AuthenticationService, TokenPayload, UserDetails } from '../authentication.service';
 import { CommonUtilityService } from '../common-utility.service';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { NotificationObject } from '../i2h-objects';
-import { environment } from '../../environments/environment';
 import { Subscription } from 'rxjs/Subscription';
 
 @Component({
@@ -12,18 +8,12 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit, OnDestroy {
-  userInfo: UserDetails;
   componentIcon = 'fa-user-circle-o';
   subscription: Subscription;
 
   constructor(
-    private auth: AuthenticationService,
-    private commonUtilityService: CommonUtilityService,
-    private router: Router,
-    private route: ActivatedRoute
+    private commonUtilityService: CommonUtilityService
   ) {
-    this.userInfo = this.auth.getLoggedInUser();
-    console.log(this.userInfo);
     this.subscription = this.commonUtilityService.getData()
     .subscribe(x => {
       this.componentIcon = x;
@@ -31,11 +21,13 @@ export class SettingsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    console.log('setngs pg', this.componentIcon);
+    this.commonUtilityService.handlePageLinkCLicks();
   }
 
   ngOnDestroy() {
     // unsubscribe to ensure no memory leaks
     this.subscription.unsubscribe();
   }
+
+
 }
