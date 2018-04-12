d54631b11280c75902ac7f50619354199a8959f3
Shammi Hans <shammihans1@gmail.com>
2018-04-12 16:14:34 +0530

    address page UI

--- UI/src/app/address/address.component.ts
+++ UI/src/app/address/address.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonUtilityService } from '../common-utility.service';
+import { UserDetails, AddressDetails } from '../i2h-objects';
+import { UserInfoService } from '../userinfo.service';
 
 @Component({
   selector: 'app-address',
@@ -8,9 +10,17 @@ import { CommonUtilityService } from '../common-utility.service';
 })
 export class AddressComponent implements OnInit, OnDestroy {
   componentIcon = 'fa-address-card';
+  userInfo: UserDetails;
+  addressName: string;
+  addressContact: string;
+  addressLine: string;
+  enabledAddBtn = false;
+  showTemplate = false;
+  addressInfo: any = [];
 
   constructor(
-    private commonUtilityService: CommonUtilityService
+    private commonUtilityService: CommonUtilityService,
+    private userInfoService: UserInfoService
   ) { }
 
   ngOnInit() {
@@ -31,4 +41,50 @@ export class AddressComponent implements OnInit, OnDestroy {
     this.commonUtilityService.clearData();
   }
 
+  handleLabelName(e) {
+    this.commonUtilityService.handleInputLabelName(e);
+  }
+  allowOnlyNumbers(e) {
+    this.commonUtilityService.allowOnlyNumbers(e);
+  }
+  pasteOnlyNumbers(e) {
+    this.commonUtilityService.pasteOnlyNumbers(e);
+  }
+
+  showAdressTemplate() {
+    this.showTemplate = true;
+    this.addressContact = this.addressName = this.addressLine = '';
+  }
+  enableAddButton(e) {
+    console.log(this.addressName, this.addressContact, this.addressLine);
+    if (this.addressName !== '' && this.addressContact !== '' && this.addressLine !== '') {
+      this.enabledAddBtn = true;
+      if (e.keyCode === 13) {
+        this.createAdress();
+      }
+    } else {
+      this.enabledAddBtn = false;
+    }
+  }
+
+  showMap() {
+    //
+  }
+  createAdress() {
+    this.showTemplate = false;
+    const addressObj = {
+      addressId: '',
+      name: this.addressName,
+      contact: this.addressContact,
+      address: this.addressLine
+    };
+    this.addressInfo.push(addressObj);
+    this.enabledAddBtn = false;
+  }
+  editAddress() {
+    //
+  }
+  deleteAddress() {
+    //
+  }
 }
